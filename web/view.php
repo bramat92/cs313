18eb0b4445db1cba8e87897ea74596d85e6ccdb1
bramat92 <bernhardt.rmt@gmail.com>
2018-02-06 11:21:26 -0700

    Hey

--- web/view.php
+++ web/view.php
@@ -33,7 +33,7 @@
 				$stmt->bindValue(':book', $variable, PDO::PARAM_STR);
 				$stmt->execute();
 				$rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
-				foreach ($db->query($stmt) as $read) {
+				foreach ($db->query($rows) as $read) {
 					echo '<p>';
 					echo '<strong>' .$read['book'] . ' ' . $read['chapter'] . ':' . $read['verse'] . '</strong> - "' . $read['content'] . '"';
 					echo '</p>';
