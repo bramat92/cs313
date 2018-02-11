5553d33072df8d8e7d4cc21d6b054c837421bcf1
bramat92 <bernhardt.rmt@gmail.com>
2018-02-10 22:55:34 -0700

    Hey

--- web/fghome.php
+++ web/fghome.php
@@ -75,7 +75,7 @@
 				$stmt = $db->prepare('SELECT * FROM users WHERE username =:name');
 				$stmt->bindValue(':name', $variable, PDO::PARAM_STR);
 				$stmt->execute();
-				foreach ($db->query('SELECT * FROM users JOIN posts ON users.id = $stmt LIMIT 10;') as $row)
+				foreach ($db->query('SELECT * FROM users JOIN posts ON users.id = "$variable" LIMIT 10;') as $row)
 				{
 					echo '<p>';
 					echo '<strong>' .$row['post'] . ' by ' . $row['username'];
