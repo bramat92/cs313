16c3d69df8ef5a1e026b99291a97c493a845b409
bramat92 <bernhardt.rmt@gmail.com>
2018-02-12 02:29:22 -0700

    Hey

--- web/fghome.php
+++ web/fghome.php
@@ -84,7 +84,7 @@
 				}
 
 				echo 'My username id is: '. $result;
-				foreach ($db->query('SELECT post, username, created_at FROM users JOIN posts ON users.id = posts.user_id WHERE users.id = (SELECT id FROM users WHERE username = \'bernie\')') as $row)
+				foreach ($db->query('SELECT post, username, posts.created_at FROM users JOIN posts ON users.id = posts.user_id WHERE users.id = (SELECT id FROM users WHERE username = \'bernie\')') as $row)
 				{
 					echo '<p>';
 					echo $row['post'] . ' posted on ' . $row['created_at'] . ' by '. $row['username'];
