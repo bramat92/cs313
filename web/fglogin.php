397c65e18d959970f64c54483a713b542a92beb4
bramat92 <bernhardt.rmt@gmail.com>
2018-02-12 06:07:32 -0700

    Hey

--- web/fglogin.php
+++ web/fglogin.php
@@ -30,6 +30,15 @@
 			h2 {
 				text-decoration: underline;
 			}
+			
+			body {
+				 background: url('friends.jpg') no-repeat center center fixed; 
+				-webkit-background-size: cover;
+				-moz-background-size: cover;
+				-o-background-size: cover;
+				background-size: cover;
+
+			}
 		</style>
 	</head>
 	<body>
