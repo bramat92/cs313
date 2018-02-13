21c78a4bdd2bfcb23bc45057b0558d3b88191d41
bramat92 <bernhardt.rmt@gmail.com>
2018-02-12 17:33:53 -0700

    Hey

--- web/fgusers.php
+++ web/fgusers.php
@@ -4,135 +4,135 @@
 		$variable = $_SESSION['uname'];		
 	}
 	try
-		{
-			$user = 'auobnrfenbtijr';
-			$password = 'd88bd9049162b1341d8970fe4ebeeae2542aade94453e76f3e73460cfbfa424c';
-			$db = new PDO('pgsql:host=ec2-54-225-103-255.compute-1.amazonaws.com;dbname=d4sni6bgp10g1t', $user, $password);
-		}
-		catch (PDOException $ex)
-		{
-			echo 'Error!: ' . $ex->getMessage();
-			die();
-		}
-?>
+			{
+				$user = 'auobnrfenbtijr';
+				$password = 'd88bd9049162b1341d8970fe4ebeeae2542aade94453e76f3e73460cfbfa424c';
+				$db = new PDO('pgsql:host=ec2-54-225-103-255.compute-1.amazonaws.com;dbname=d4sni6bgp10g1t', $user, $password);
+			}
+			catch (PDOException $ex)
+			{
+				echo 'Error!: ' . $ex->getMessage();
+				die();
+			}
+	?>
 
-<!DOCTYPE html>
-<html lang="en">
-	<head>
-		<!-- Required meta tags -->
-		<meta charset="utf-8">
-		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<!DOCTYPE html>
+	<html lang="en">
+		<head>
+			<!-- Required meta tags -->
+			<meta charset="utf-8">
+			<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-		<!-- Bootstrap CSS -->
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+			<!-- Bootstrap CSS -->
+			<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 
-		<title>Friendstagram</title>
-		<style>
-			h1 {
-				text-align: center;
-				text-decoration: underline;
-				margin: 50px;
-			}
-			body {
-				 background: url('friends.jpg') no-repeat center center fixed; 
-				-webkit-background-size: cover;
-				-moz-background-size: cover;
-				-o-background-size: cover;
-				background-size: cover;
-				
-			}
-			nav {
-				border-radius: 5px;
-			}
-			
-			#welcome {
-				text-align: center;
-				margin-top: 20px;
-				font-size: 50px;
-				
-			}
-			#friendstagram {
-				font-size: 80px;
-				text-align: center;
-				text-decoration: underline;
-			}
-			#name {
-				font-size: 30px;
-				text-align: center;
-				margin-bottom: 20px;
-			}
-			#displays {
-				margin-left: auto;
-				margin-right: auto;
-				margin-bottom: 10px;
-				width: 700px;
-				display: inline-block;
-			}
-		</style>
-	</head>
-	<body>
-		<p id="friendstagram">Friendstagram</p>
-		<div class="container">
-			<nav class="navbar navbar-expand-lg navbar-light bg-light">
-				<a class="navbar-brand" href="#">FG</a>
-				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-					<span class="navbar-toggler-icon"></span>
-				</button>
-
-				<div class="collapse navbar-collapse" id="navbarSupportedContent">
-					<ul class="navbar-nav mr-auto">
-						<li class="nav-item active">
-							<a class="nav-link" href="fghome.php">Home <span class="sr-only">(current)</span></a>
-						</li>
-						<li class="nav-item">
-							<a class="nav-link" href="fgposts.php">Posts</a>
-						</li>
-						<li class="nav-item">
-							<a class="nav-link" href="followees.php">Followers</a>
-						</li>
-						<li class="nav-item">
-							<a class="nav-link" href="fgfollowing.php">Following</a>
-						</li>
-						<li class="nav-item">
-							<a class="nav-link" href="fgusers.php">Users</a>
-						</li>
-
-						
-					</ul>
-					<form class="form-inline my-2 my-lg-0">
-						<input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-						<button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-					</form>
-				</div>
-			</nav>
-			<p id = "welcome">Welcome</p>
-			<?php
-				
-				$stmt = $db->prepare('SELECT * FROM users WHERE username=:name');
-				$stmt->bindValue(':name', $variable, PDO::PARAM_STR);
-				$stmt->execute();
-				foreach ($stmt->fetchAll(PDO::FETCH_ASSOC) as $rows)
-				{
-					echo '<p id = "name">';
-					echo ' Logged in as: '$rows['firstname'] . ' ' . $rows['lastname']; 
-					echo '</p>';
+			<title>Friendstagram</title>
+			<style>
+				h1 {
+					text-align: center;
+					text-decoration: underline;
+					margin: 50px;
+				}
+				body {
+					 background: url('friends.jpg') no-repeat center center fixed; 
+					-webkit-background-size: cover;
+					-moz-background-size: cover;
+					-o-background-size: cover;
+					background-size: cover;
+					
+				}
+				nav {
+					border-radius: 5px;
 				}
 				
-				foreach ($db->query('SELECT username, firstname, lastname, to_char(users.created_at, \'YYYY/MM/DD\') AS date FROM users') as $rows)
-				{
-					echo '<div class="alert alert-secondary" id = "displays" role="alert">';
-					echo 'Username: ' . $rows['username'] . '<br>' . $rows['firstname'] . ' ' . $rows['lastname'];
-					echo '<br> Member since: ' . $rows['date']; 
-					echo '</div>';
-					echo '<br>';
+				#welcome {
+					text-align: center;
+					margin-top: 20px;
+					font-size: 50px;
+					
 				}
-			?>
-		</div>
+				#friendstagram {
+					font-size: 80px;
+					text-align: center;
+					text-decoration: underline;
+				}
+				#name {
+					font-size: 30px;
+					text-align: center;
+					margin-bottom: 20px;
+				}
+				#displays {
+					margin-left: auto;
+					margin-right: auto;
+					margin-bottom: 10px;
+					width: 700px;
+					display: inline-block;
+				}
+			</style>
+		</head>
+		<body>
+			<p id="friendstagram">Friendstagram</p>
+			<div class="container">
+				<nav class="navbar navbar-expand-lg navbar-light bg-light">
+					<a class="navbar-brand" href="#">FG</a>
+					<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+						<span class="navbar-toggler-icon"></span>
+					</button>
 
-		<!-- Optional JavaScript -->
-		<!-- jQuery first, then Popper.js, then Bootstrap JS -->
-		<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-	</body>
-</html>
+					<div class="collapse navbar-collapse" id="navbarSupportedContent">
+						<ul class="navbar-nav mr-auto">
+							<li class="nav-item active">
+								<a class="nav-link" href="fghome.php">Home <span class="sr-only">(current)</span></a>
+							</li>
+							<li class="nav-item">
+								<a class="nav-link" href="fgposts.php">Posts</a>
+							</li>
+							<li class="nav-item">
+								<a class="nav-link" href="followees.php">Followers</a>
+							</li>
+							<li class="nav-item">
+								<a class="nav-link" href="fgfollowing.php">Following</a>
+							</li>
+							<li class="nav-item">
+								<a class="nav-link" href="fgusers.php">Users</a>
+							</li>
+
+							
+						</ul>
+						<form class="form-inline my-2 my-lg-0">
+							<input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+							<button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+						</form>
+					</div>
+				</nav>
+				<p id = "welcome">Welcome</p>
+				<?php
+					
+					$stmt = $db->prepare('SELECT * FROM users WHERE username=:name');
+					$stmt->bindValue(':name', $variable, PDO::PARAM_STR);
+					$stmt->execute();
+					foreach ($stmt->fetchAll(PDO::FETCH_ASSOC) as $rows)
+					{
+						echo '<p id = "name">';
+						echo $rows['firstname'] . ' ' . $rows['lastname']; 
+						echo '</p>';
+					}
+					
+					foreach ($db->query('SELECT post, firstname, lastname, to_char(posts.created_at, \'YYYY/MM/DD\') AS date FROM posts JOIN users ON users.id = posts.user_id ORDER BY date DESC') as $rows)
+					{
+						echo '<div class="alert alert-secondary" id = "displays" role="alert">';
+						echo $rows['firstname'] . ' ' . $rows['lastname'] . '<br>'; 
+						echo $rows['post'] . '<br>'. '"' . $rows['date'] . '"';
+						echo '</div>';
+						echo '<br>';
+					}
+				?>
+			</div>
+
+			<!-- Optional JavaScript -->
+			<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+			<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+			<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+			<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+		</body>
+	</html>
