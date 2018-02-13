cdf454478c7ae1395c5f999c9e657747927505b7
bramat92 <bernhardt.rmt@gmail.com>
2018-02-13 11:16:24 -0700

    Hey

--- web/view.php
+++ web/view.php
@@ -28,9 +28,9 @@
 			$stmt->bindValue(':content', $content, PDO::PARAM_STR);
 			$stmt->execute();	
 			
-		$newid = $pdo->PDO::lastInsertId('scripture_id_seq');
+		$newid = $pdo->lastInsertId('scripture_id_seq');
 		
-		$td = $db->query('SELECT id FROM topic WHERE name = :tpname');
+		$td = $db->prepare('SELECT id FROM topic WHERE name = :tpname');
 		$td->bindValue(':tpname', $topicname, PDO::PARAM_STR);
 		$td->execute();
 		$row = $td->fetchAll(PDO::FETCH_ASSOC);
