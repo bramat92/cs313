499dda81e03abed8ea72aa2a89df39e9f7deaed1
bramat92 <bernhardt.rmt@gmail.com>
2018-02-12 17:45:15 -0700

    Hey

--- web/fghome.php
+++ web/fghome.php
@@ -79,7 +79,7 @@
 		<p id="friendstagram">Friendstagram</p>
 		<div class="container">
 			<nav class="navbar navbar-expand-lg navbar-light bg-light">
-				<a class="navbar-brand" href="#">FG</a>
+				<a class="navbar-brand" href="fglogin.php">FG</a>
 				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 					<span class="navbar-toggler-icon"></span>
 				</button>
