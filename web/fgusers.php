220e3870a5dd2b6b9c5c3eec1d5fbc51ba6ff29b
bramat92 <bernhardt.rmt@gmail.com>
2018-02-12 17:43:26 -0700

    Hey

--- web/fgusers.php
+++ web/fgusers.php
@@ -118,11 +118,11 @@
 						echo '</p>';
 					}
 					
-					foreach ($db->query('SELECT * FROM users') as $rows)
+					foreach ($db->query('SELECT username, firstname, lastname, to_char(posts.created_at, \'YYYY/MM/DD\') AS date FROM users') as $rows)
 					{
 						echo '<div class="alert alert-secondary" id = "displays" role="alert">';
 						echo 'Username: ' . $rows['username'] . '<br>' . $rows['firstname'] . ' ' . 
-						$rows['lastname'] . '<br> Member since: ' . $rows['to_char(posts.created_at, \'YYYY/MM/DD\')']; 
+						$rows['lastname'] . '<br> Member since: ' . $rows['date']; 
 						echo '</div>';
 						echo '<br>';
 					}
