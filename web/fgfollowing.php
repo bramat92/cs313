87d486a30bfd9173c65ea174adec595d83a6ef98
bramat92 <bernhardt.rmt@gmail.com>
2018-03-26 04:26:48 -0600

    New Changes

--- web/fgfollowing.php
+++ web/fgfollowing.php
@@ -177,7 +177,7 @@
 					echo $rows['firstname'] . ' ' . $rows['lastname'] . '<br>'; 
 					echo '<em>Since ' . $rows['date'] . '</em>';
 					echo '<br>';
-					echo '<form action="followees.php" method="get">
+					echo '<form action="fgfollowing.php" method="get">
 						<input type="hidden" name="unfid" value="'. $rows['uid'] .'">
 						<button type="submit" id="lb" name="unfollow" class="btn btn-primary">Unfollow</button>
 					</form>';
