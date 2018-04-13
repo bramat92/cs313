a9da751ec4f9bcefae283800fe4c8240797f3bc9
bramat92 <bernhardt.rmt@gmail.com>
2018-04-13 01:31:04 -0600

    new search

--- web/fghome.php
+++ web/fghome.php
@@ -324,7 +324,7 @@
 				
 				if (isset($_GET['sbtn'])){
 					$text = $_GET['search'];
-					$ptext = $db->prepare('SELECT post, CONCAT(firstname, " ", lastname) AS name, to_char(posts.created_at, \'YYYY/MM/DD\') AS date FROM posts JOIN users ON users.id = posts.user_id WHERE name LIKE :keyword ORDER BY date DESC');
+					$ptext = $db->prepare('SELECT post, CONCAT(firstname, \' \', lastname) AS name, to_char(posts.created_at, \'YYYY/MM/DD\') AS date FROM posts JOIN users ON users.id = posts.user_id WHERE (post LIKE :keyword) OR (CONCAT(firstname, \' \', lastname) LIKE :keyword) ORDER BY date DESC');
 					$text = "%".$text."%";
 					$ptext->bindValue(':keyword', $text, PDO::PARAM_STR);
 					$ptext->execute();
