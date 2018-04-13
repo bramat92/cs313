e437c354c05b550ab8e97b0ca6cdef7b17ee9068
bramat92 <bernhardt.rmt@gmail.com>
2018-04-13 02:35:45 -0600

    new search and look

--- web/fgusers.php
+++ web/fgusers.php
@@ -110,6 +110,9 @@
 					margin-right: auto;
 					max-width: 50%;
 					height: auto;
+					box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+					border-radius: 5px;
+					margin-bottom: 20px;
 				}
 				#lb {
 					box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
